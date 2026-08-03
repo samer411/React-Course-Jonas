bc3a68ac9960d56667c263e88a293659983b37ed
Samer Nassim <samer.nassim.behery@gmail.com>
2026-08-03 22:08:46 +0300

    Refactor application structure by moving components to a dedicated folder and implementing item management functionality

---
+++ 05-travel-list/src/components/Form.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function Form({onAddItems}) {
+  const [description, setDescription] = useState("");
+  const [selectState, setSelectState] = useState(1);
+
+  
+  function handleSubmit(e){
+    e.preventDefault();
+
+    if (!description) return;
+    const newItem = { description, selectState, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription((d) => (d = ""));
+    setSelectState((s) => (s = 1));
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?😍</h3>
+      <select
+        value={selectState}
+        onChange={(e) => setSelectState(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <button>Add</button>
+    </form>
+  );
+}
--- 05-travel-list/src/main.jsx
+++ 05-travel-list/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";4
 import { StrictMode } from "react";
-import App from './App'
+import App from './components/App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
